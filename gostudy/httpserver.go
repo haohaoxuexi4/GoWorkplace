ce8293e09bdf01d7129a8ea6728e98194104f826
xufeng <xufengyongyou@163.com>
2017-05-19 16:08:51 +0800

    增加了访问计数功能，和锁

--- gostudy/httpserver.go
+++ gostudy/httpserver.go
@@ -3,12 +3,26 @@ import(
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+var count int
+var mu sync.Mutex
 func handler(w http.ResponseWriter,r *http.Request){
+	mu.Lock()
+	count++
+	fmt.Println("handle")
+	//fmt.Fprintln(count)
+	mu.Unlock()
 	fmt.Fprintf(w,"urlpath=%q\n",r.URL.Path)
+}
+func counter(w http.ResponseWriter,r *http.Request){
+	mu.Lock()
+	fmt.Fprintf(w,"count=%d\n",count)
+	mu.Unlock()
 }
 func main(){
 
 	http.HandleFunc("/",handler)
+	http.HandleFunc("/count",counter)
 	log.Fatal(http.ListenAndServe("localhost:8080",nil))
 }
