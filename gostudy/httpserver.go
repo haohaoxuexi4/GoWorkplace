5cc415c5acb61b56fc16cc5ca2bffbc076c16055
xufeng <xufengyongyou@163.com>
2017-05-19 15:55:07 +0800

    一个最简单的httpserver
    用法：浏览器打开localhost:8080

---
+++ gostudy/httpserver.go
@@ -0,0 +1,14 @@
+package main
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+func handler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"urlpath=%q\n",r.URL.Path)
+}
+func main(){
+
+	http.HandleFunc("/",handler)
+	log.Fatal(http.ListenAndServe("localhost:8080",nil))
+}
